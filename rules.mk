f00f85790d29c111f1e4709484a8d655e7af38b6
Mike Lubinets <lubinets@peaq.io>
2021-03-27 02:02:02 +0100

    disable oryx and webusb

--- rules.mk
+++ rules.mk
@@ -1,8 +1,8 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 COMMAND_ENABLE = no
-WEBUSB_ENABLE = yes
-ORYX_ENABLE = yes
+WEBUSB_ENABLE = no
+ORYX_ENABLE = no
 SRC = matrix.c
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
